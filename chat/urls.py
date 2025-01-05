ad95602933580b8b683fcef62bf986c4c4b99b70
ArielSt95V2 <astablediffusion2@gmail.com>
2025-01-05 10:32:32 +0200

    starting to work with Ducker -- initial state 05.01.25

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import ChatHistoryListCreateView, ChatMessageView, CleanTextView
+from .views import ChatHistoryListCreateView, ChatMessageView
 
 urlpatterns = [
     path('history/', ChatHistoryListCreateView.as_view(), name='chat-history'),
     path('message/', ChatMessageView.as_view(), name='chat-message'),
-
-    path('clean-text/', CleanTextView.as_view(), name='clean-text'),
 ]
